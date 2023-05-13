e920d9dd1e237ad888f5f76b2a9a41c651c97b31
Rihard12 <rs1027@student.uni-lj.si>
2023-05-13 22:24:47 +0200

    kinda najs, ne se cist kul

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,30 +1,29 @@
 import * as React from "react";
+import { useState } from 'react'
 import { FormSubmitButton, FormInput } from "@builder.io/react";
 
+
 export default function Body(props) {
+  const [sliderValue, setSliderValue] = useState(40)
+
   return (
     <>
       <div
         className="flex flex-col relative shrink-0 box-border h-auto grow-0 border mt-0 p-[20px] rounded-[5px] border-none"
-        maxWidth={1200}
-        lazyLoad={false}
       >
         <section className="flex flex-col relative shrink-0 box-border h-auto grow-0 border mt-0 p-[20px] rounded-[5px] border-none section">
           <div
             className="flex flex-row justify-around items-stretch gap-[10px] m-[30px] px-[50px]"
             space={30}
           >
-            <FormInput
+            <input
               type="text"
               placeholder="What would you like to protest about?"
               name="type"
               className="flex flex-col relative shrink-0 box-border self-stretch h-auto grow mr-auto pl-[20px] pr-0 py-[20px] rounded-[10px] border-[3px] border-solid border-[#ccc]"
               required={true}
             />
-            <FormSubmitButton
-              text="Search"
-              className="inline-flex flex-row items-center justify-center relative shrink-0 box-border no-underline appearance-none bg-[#3898EC] text-[white] cursor-pointer self-stretch leading-none transition-[background-color,border-color,color,fill,stroke,opacity,box-shadow,transform] duration-[0.2s] ease-in-out delay-[0s] whitespace-nowrap font-medium text-[16px] -tracking-wider overflow-hidden text-ellipsis normal-case align-baseline h-auto mr-auto px-[24px] py-[12px] rounded-[5px] border-[none]"
-            />
+            <button className="h-12 px-6 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">Submit</button>
           </div>
           <div className="relative shrink-0 box-border flex flex-row w-auto self-stretch justify-around gap-[20px] mt-[20px]">
             <div className="flex flex-col relative shrink-0 box-border leading-[normal] h-auto ml-auto mt-[20px]">
@@ -37,27 +36,16 @@ export default function Body(props) {
               <p>Very serious</p>
             </div>
           </div>
+          <div className="px-36 py-5 w-full">
+            <input type="range" min="0" max="100" value={sliderValue} onChange={e => setSliderValue(e.target.value)} className="range w-full" />
+          </div>
           <div
             className="flex flex-col relative shrink-0 box-border min-h-[180px] w-full mt-0 mx-auto pl-[7px] pr-[20px] pt-[10px] pb-[20px]"
-            maxWidth={1200}
-            lazyLoad={false}
           >
             <section className="flex flex-col relative shrink-0 box-border min-h-[180px] w-full mt-0 mx-auto pl-[7px] pr-[20px] pt-[10px] pb-[20px] section" />
           </div>
         </section>
       </div>
-      <style jsx>{`
-        .section {
-          width: 100%;
-          align-self: stretch;
-          flex-grow: 1;
-          max-width: 1200px;
-          display: flex;
-          flex-direction: column;
-          margin-left: auto;
-          margin-right: auto;
-        }
-      `}</style>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,33 +3,13 @@ import * as React from "react";
 export default function Header(props) {
   return (
     <>
-      <div
-        className="flex flex-col relative shrink-0 box-border min-h-[100px] bg-[rgba(0,39,128,0.8)] leading-[normal] h-auto text-left text-[25px] mx-[15px] my-[10px] p-[20px]"
-        maxWidth={1200}
-        lazyLoad={false}
-      >
-        <section className="flex flex-col relative shrink-0 box-border min-h-[100px] bg-[rgba(0,39,128,0.8)] leading-[normal] h-auto text-left text-[25px] mx-[15px] my-[10px] p-[20px] section">
-          <h1>
-            <strong>
-              <em>
-                DIGITAL PROTESTS<span class="ql-cursor">﻿</span>
-              </em>
-            </strong>
-          </h1>
-        </section>
-      </div>
-      <style jsx>{`
-        .section {
-          width: 100%;
-          align-self: stretch;
-          flex-grow: 1;
-          max-width: 1200px;
-          display: flex;
-          flex-direction: column;
-          margin-left: auto;
-          margin-right: auto;
-        }
-      `}</style>
+      <section className="min-w-full h-screen max-h-36 flex flex-col relative shrink-0 box-border bg-[rgba(0,39,128,0.8)] leading-[normal] h-auto text-left text-[25px] mx-[15px] section">
+        <h1 className="p-12 font-bold">
+            <em>
+              DIGITAL PROTESTS<span className="ql-cursor">🤰</span>
+            </em>
+        </h1>
+      </section>
     </>
   );
 }
