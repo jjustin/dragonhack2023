d4dc5482218dffb66c9aa1f16186388d36f4157d
Janez Justin <janez.justin0@gmail.com>
2023-05-14 10:55:36 +0200

    header fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,28 +2,28 @@ import * as React from "react";
 import "./Header.css";
 
 export default function Header(props) {
-  const navigate = (to) => {
-    window.location.href = to;
-  };
   return (
     <>
       <section
         style={{ backgroundColor: "rgba(255, 78, 94, 1)" }}
         className="header min-w-full h-screen max-h-32 flex flex-col relative shrink-0 box-border leading-normal text-left mx-8 section"
       >
-        <h1
-          onClick={() => navigate("")}
-          className="px-12 pt-4 font-bold font-mono text-5xl text-white text-black"
-          style={{ cursor: "pointer" }}
-        >
-          <span
-            className="animate-bounce text-7xl text-white"
-            style={{ fontFamily: "Roboto, sans-serif" }}
+        <a href="/">
+          <h1
+            className="px-12 pt-4 font-bold font-mono text-5xl text-white text-black"
+            style={{ cursor: "pointer" }}
           >
-            Spark
-          </span>
-          <div className="text-xs pt-2 pl-1 text-white">Ignite your voice</div>
-        </h1>
+            <span
+              className="animate-bounce text-7xl text-white"
+              style={{ fontFamily: "Roboto, sans-serif" }}
+            >
+              Spark
+            </span>
+            <div className="text-xs pt-2 pl-1 text-white">
+              Ignite your voice
+            </div>
+          </h1>
+        </a>
       </section>
     </>
   );
